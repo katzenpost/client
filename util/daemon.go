4b31da5ab0067fc1facc2accb795175e5855f565
David Stainton <dstainton415@gmail.com>
2017-07-29 00:17:07 +0000

    Remove commented out code

--- util/daemon.go
+++ util/daemon.go
@@ -32,25 +32,20 @@ var log = logging.MustGetLogger("mixclient")
 
 // ClientDaemon handles the startup and shutdown of all client services
 type ClientDaemon struct {
-	config *Config
 	//session *common.Session
 	conn net.Conn
 }
 
 // NewClientDaemon creates a new ClientDaemon given a Config
 func NewClientDaemon(config *Config) *ClientDaemon {
-	// sessionConfig := common.Config{
-	// 	Initiator:  true,
-	// 	Identifier: config.Identifier,
-	// 	Random:     rand.Reader,
-	// 	//LongtermEd25519PublicKey:  config.PublicEd25519Key,
-	// 	//LongtermEd25519PrivateKey: config.PrivateEd25519Key,
-	// }
-	c := ClientDaemon{
-		config: config,
-		//session: common.New(&sessionConfig, nil),
+
+	sessionConfig := wire.SessionConfig{}
+	session, err := NewSession(&sessionConfig, true)
+
+	d := ClientDaemon{
+	//session: common.New(&sessionConfig, nil),
 	}
-	return &c
+	return &d
 }
 
 // Start starts the client services
@@ -65,11 +60,6 @@ func (c *ClientDaemon) Start() error {
 		panic(err)
 	}
 
-	// err = c.Dial(c.config.ProviderNetwork, c.config.ProviderAddress)
-	// if err != nil {
-	// 	log.Debugf("dial failed: %s", err)
-	// 	return err
-	// }
 	return nil
 }
 
@@ -78,31 +68,3 @@ func (c *ClientDaemon) Stop() {
 	// XXX fix me
 	log.Debug("Client shutdown.")
 }
-
-// func (c *ClientDaemon) Dial(network, address string) error {
-// 	var err error
-// 	c.conn, err = net.Dial(network, address)
-// 	if err != nil {
-// 		log.Notice("failed to dial provider")
-// 		return err
-// 	}
-// 	return c.session.Initiate(c.conn)
-// }
-
-// func (c *ClientDaemon) Read() (*common.Command, error) {
-// 	cmd, err := c.session.Receive()
-// 	if err != nil {
-// 		log.Debugf("session read error: %s", err)
-// 		return nil, err
-// 	}
-// 	return &cmd, nil
-// }
-
-// func (c *ClientDaemon) Write(cmd *common.Command) error {
-// 	err := c.session.Send(*cmd)
-// 	if err != nil {
-// 		log.Debugf("session write error: %s", err)
-// 		return err
-// 	}
-// 	return nil
-// }
