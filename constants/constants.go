c7080f0ee09da40d7ceae1fc8b9355ba159e5edc
David Stainton <dstainton415@gmail.com>
2017-09-15 07:23:50 +0000

    Add more constants

--- constants/constants.go
+++ constants/constants.go
@@ -17,7 +17,43 @@
 // Package constants contains the client constants for Katzenpost.
 package constants
 
+import (
+	"time"
+)
+
 const (
+	// DatabaseConnectTimeout is a duration used as the connect timeout
+	// when we access our local databases (for POP3&SMTP proxies)
+	DatabaseConnectTimeout = 3 * time.Second
+
+	// PoissonLambda is the poisson distribution's lambda parameter
+	// used for setting mix hop delays
+	// XXX fix me
+	PoissonLambda = float64(.234)
+
+	// HopsPerPath is the number of mix hops per path through the mix network
+	HopsPerPath = 3
+
 	// MessageIDLength is the length of a message ID in bytes.
 	MessageIDLength = 16
+
+	// EndToEndKeyType is the string representing the end to end
+	// messaging key type
+	EndToEndKeyType = "e2e"
+
+	// LinkLayerKeyType is the string representing the link layer
+	// wire protocol key type
+	LinkLayerKeyType = "wire"
+
+	// DefaultSMTPNetwork is the default network type used for our SMTP proxy service
+	DefaultSMTPNetwork = "tcp"
+
+	// DefaultSMTPAddress is the default address used for our SMTP proxy service
+	DefaultSMTPAddress = "127.0.0.1:2525"
+
+	// DefaultPOP3Network is the default network type used for our POP3 proxy service
+	DefaultPOP3Network = "tcp"
+
+	// DefaultPOP3Address is the default address type used for our POP3 proxy service
+	DefaultPOP3Address = "127.0.0.1:1110"
 )
