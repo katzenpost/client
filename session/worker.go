e0c0bd6199206dc6103d9df810f03bfabfe7484f
Masala <masala@riseup.net>
2019-03-01 16:59:13 -0500

    obtain lock protecting egressQueue

--- session/worker.go
+++ session/worker.go
@@ -134,6 +134,8 @@ func (s *Session) worker() {
 func (s *Session) lambdaPTask() {
 	// Attempt to send user data first, if any exists.
 	// Otherwise send a drop decoy message.
+	s.egressQueueLock.Lock()
+	defer s.egressQueueLock.Unlock()
 	_, err := s.egressQueue.Peek()
 	if err == nil {
 		err := s.sendNext()
